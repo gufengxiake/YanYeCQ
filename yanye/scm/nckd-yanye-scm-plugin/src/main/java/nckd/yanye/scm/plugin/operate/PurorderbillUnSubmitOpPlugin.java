a1d0fdbc1aff4a86031baa165d0edeba0a024ce8
zhujintao <983068902@qq.com>
2024-07-18 14:36:40 +0800

    add:增加采购订单的提交和撤销操作插件      * 采购订单如已匹配采购合同，则采购订单提交时，将该采购订单明细行的 “数量”及“价税合计”以累计增加的方式反写至采购合同对应行的“已下达订单数”及“已下达订单金额”，      * 同时触发采购合同财务信息“已执行价税合计”字段进行累计汇总，如果采购订单撤销，则进行扣减对应字段；

--- yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/operate/PurorderbillUnSubmitOpPlugin.java
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/operate/PurorderbillUnSubmitOpPlugin.java
@@ -9,19 +9,21 @@ import kd.bos.entity.plugin.args.BeginOperationTransactionArgs;
 import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.bos.servicehelper.operation.SaveServiceHelper;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Module           :供应链云-采购管理-采购订单
- * Description      :采购订单提交操作插件
+ * Description      :采购订单撤销操作插件
  *
  * @author : zhujintao
  * @date : 2024/7/16
  */
-public class PurorderbillOpPlugin extends AbstractOperationServicePlugIn {
+public class PurorderbillUnSubmitOpPlugin extends AbstractOperationServicePlugIn {
     @Override
     public void onPreparePropertys(PreparePropertysEventArgs e) {
         super.onPreparePropertys(e);
@@ -31,6 +33,7 @@ public class PurorderbillOpPlugin extends AbstractOperationServicePlugIn {
     }
 
     /**
+     * 提交时是触发下面的逻辑，撤销则是下面的逻辑反过来
      * 采购订单如已匹配采购合同，则采购订单提交时，将该采购订单明细行的 “数量”及“价税合计”以累计增加的方式反写至采购合同对应行的“已下达订单数”及“已下达订单金额”，
      * 同时触发采购合同财务信息“已执行价税合计”字段进行累计汇总，如果采购订单撤销，则进行扣减对应字段；
      */
@@ -51,7 +54,7 @@ public class PurorderbillOpPlugin extends AbstractOperationServicePlugIn {
                 .and("validstatus", QCP.equals, "B")
                 .and("closestatus", QCP.equals, "A");
         //String entityName, String selectProperties, QFilter[] filters
-        DynamicObject[] purcontractArr = BusinessDataServiceHelper.load("conm_purcontract", "id,billno,totalallamount,nckd_totalallamount,nckd_totalallamountrat,billentry.id,billentry.qty,billentry.nckd_qty,billentry.nckd_qtyratio,billentry.amountandtax,billentry.nckd_amount,billentry.nckd_amountratio,billentry.seq", qFilter.toArray());
+        DynamicObject[] purcontractArr = BusinessDataServiceHelper.load("conm_purcontract", "id,billno,totalallamount,nckd_totalallamount,nckd_totalallamountrat,billentry.id,billentry.material,billentry.qty,billentry.nckd_qty,billentry.nckd_qtyratio,billentry.amountandtax,billentry.nckd_amount,billentry.nckd_amountratio,billentry.seq", qFilter.toArray());
         //转成key id value DynamicObject采购合同
         Map<Long, DynamicObject> map = Arrays.stream(purcontractArr).collect(Collectors.toMap(k -> k.getLong("id"), v -> v));
         //遍历提交的采购订单单据 进行逻辑处理
@@ -83,7 +86,20 @@ public class PurorderbillOpPlugin extends AbstractOperationServicePlugIn {
                     //以下字段来源于采购订单
                     BigDecimal qty1 = entry.getBigDecimal("qty");
                     BigDecimal amountandtax1 = entry.getBigDecimal("amountandtax");
-                    int i = 1/0;
+                    //采购合同已下单数量=采购合同的已下单数量+采购订单的数量
+                    purcontractBillEntryDy.set("nckd_qty", nckdQty.subtract(qty1));
+                    //采购合同已下单数量比例=采购合同的已下单数量/采购合同的数量
+                    purcontractBillEntryDy.set("nckd_qtyratio", nckdQty.subtract(qty1).divide(qty, 4, RoundingMode.HALF_UP).multiply(new BigDecimal(100)));
+                    //采购合同已下单金额=采购合同的已下单金额+采购订单的价税合计
+                    purcontractBillEntryDy.set("nckd_amount", nckdAmount.subtract(amountandtax1));
+                    //采购合同已下单金额比例=采购合同的已下单金额/采购合同的价税合计
+                    purcontractBillEntryDy.set("nckd_amountratio", nckdAmount.subtract(amountandtax1).divide(amountandtax, 4, RoundingMode.HALF_UP).multiply(new BigDecimal(100)));
+                    //已执行价税合计=采购合同的已下单金额求和
+                    BigDecimal sumNckdAmount = purcontractBillEntry.stream().map(k -> k.getBigDecimal("nckd_amount")).reduce(BigDecimal.ZERO, BigDecimal::add);
+                    purcontractBill.set("nckd_totalallamount", sumNckdAmount);
+                    //已执行价税合计比例=采购合同的已下单金额求和/财务信息的价税合计
+                    purcontractBill.set("nckd_totalallamountrat", sumNckdAmount.divide(totalallamount, 4, RoundingMode.HALF_UP).multiply(new BigDecimal(100)));
+                    SaveServiceHelper.save(purcontractArr);
                 }
             }
         }
