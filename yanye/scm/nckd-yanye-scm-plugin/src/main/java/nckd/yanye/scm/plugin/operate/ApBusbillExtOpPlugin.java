f68a5d1731b94ddcdda13cf4147441eb472d97d4
husheng <yeyushenghs@163.com>
2024-07-22 18:10:01 +0800

    暂估应付单保存时生成单据编号_行号和供应商

---
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/operate/ApBusbillExtOpPlugin.java
@@ -0,0 +1,31 @@
+package nckd.yanye.scm.plugin.operate;
+
+import java.util.Arrays;
+
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.entity.plugin.AbstractOperationServicePlugIn;
+import kd.bos.entity.plugin.args.BeginOperationTransactionArgs;
+
+/**
+ * @author husheng
+ * @date 2024-07-18 16:43
+ * @description  暂估应付单保存插件
+ */
+public class ApBusbillExtOpPlugin extends AbstractOperationServicePlugIn {
+
+    @Override
+    public void beginOperationTransaction(BeginOperationTransactionArgs e) {
+        DynamicObject[] dataEntities = e.getDataEntities();
+
+        Arrays.stream(dataEntities).forEach(i -> {
+            String billno = (String) i.get("billno");
+            DynamicObject asstact = i.getDynamicObject("asstact");
+            DynamicObjectCollection entry = i.getDynamicObjectCollection("entry");
+            for (DynamicObject dynamicObject : entry) {
+                dynamicObject.set("nckd_billnumber", billno + "_" + dynamicObject.get("seq"));
+                dynamicObject.set("nckd_supplier", asstact.get("id"));
+            }
+        });
+    }
+}
