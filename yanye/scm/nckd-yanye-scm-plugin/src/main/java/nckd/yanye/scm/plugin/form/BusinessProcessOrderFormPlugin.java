a7c990b2472c7e2cbce8e8cd5ab9b9fa2e0dbb7c
yaosijie <1783630922@qq.com>
2024-07-23 14:22:30 +0800

    物料-业务处理对应单单据插件开发

---
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/form/BusinessProcessOrderFormPlugin.java
@@ -0,0 +1,51 @@
+package nckd.yanye.scm.plugin.form;
+
+
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.form.field.BasedataEdit;
+import kd.bos.form.field.events.BeforeF7SelectEvent;
+import kd.bos.form.field.events.BeforeF7SelectListener;
+import kd.bos.form.plugin.AbstractFormPlugin;
+import kd.bos.orm.query.QCP;
+import kd.bos.orm.query.QFilter;
+import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.sdk.plugin.Plugin;
+
+import java.util.*;
+/**
+ * Module           :制造云-生产任务管理-生产工单（新）
+ * Description      :物料-业务处理对应单单据插件
+ *
+ * @author : yaosijie
+ * @date : 2024/7/23
+ */
+public class BusinessProcessOrderFormPlugin extends AbstractFormPlugin implements Plugin, BeforeF7SelectListener {
+
+    @Override
+    public void registerListener(EventObject e) {
+        //注册基础资料点击前的监听
+        BasedataEdit basedataEdit = this.getView().getControl("nckd_materielfield");
+        basedataEdit.addBeforeF7SelectListener(this);
+        super.registerListener(e);
+    }
+
+    @Override
+    public void beforeF7Select(BeforeF7SelectEvent beforeF7SelectEvent) {
+        String name = beforeF7SelectEvent.getProperty().getName();
+        if (name.equals("nckd_materielfield")){
+            List<QFilter> qFilters = new ArrayList<>();
+            //构造物料库存信息查询条件（"1" 表示物料允许负库存）
+            QFilter qFilter = new QFilter("isallowneginv", QCP.equals, "1");
+            DynamicObject[] dynamicObjects = BusinessDataServiceHelper.load("bd_materialinventoryinfo", "id,masterid", new QFilter[]{qFilter});
+            Set<Long> mates = new HashSet<>();
+            Arrays.stream(dynamicObjects).forEach(t->{
+                DynamicObject dynamicObject = t.getDynamicObject("masterid");
+                mates.add(dynamicObject.getLong("masterid"));
+            });
+            //构造物料信息查询条件
+            QFilter MetesqFilter = new QFilter("id", QCP.in, mates);
+            qFilters.add(MetesqFilter);
+            beforeF7SelectEvent.setCustomQFilters(qFilters);
+        }
+    }
+}
