09edcb2c77066a5a50a7176cb72c762ff76921a0
yaosijie <1783630922@qq.com>
2024-07-31 16:48:45 +0800

    插件修改

--- yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/form/NegainventoryOrderListPlugin.java
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/form/NegainventoryOrderListPlugin.java
@@ -10,7 +10,6 @@ import kd.bos.entity.botp.runtime.PushArgs;
 import kd.bos.entity.datamodel.ListSelectedRow;
 import kd.bos.entity.datamodel.ListSelectedRowCollection;
 import kd.bos.entity.operate.result.OperationResult;
-import kd.bos.exception.KDBizException;
 import kd.bos.form.events.AfterDoOperationEventArgs;
 import kd.bos.list.BillList;
 import kd.bos.list.plugin.AbstractListPlugin;
@@ -159,6 +158,7 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         invcountscheme.set("dept",dynamicObject.getDynamicObject("nckd_inventoryorg"));//库管部门
         invcountscheme.set("settlecurrency",1);//结算币别
         invcountscheme.set("bookdate",inventoryDynamicObject.getDate("nckd_inventoryclosedate"));//记账日期
+        invcountscheme.set("asyncstatus","B");//异步状态
 
         invcountscheme.set("creator", RequestContext.get().getCurrUserId());
         invcountscheme.set("createtime",new Date());
@@ -174,6 +174,7 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         QFilter qFilter = new QFilter("masterid",QCP.equals,materieNumber);
         DynamicObject material = BusinessDataServiceHelper.loadSingle("bd_materialinventoryinfo", qFilter.toArray());
         negainventoryOrderEntry.set("material",material);//物料编码
+        negainventoryOrderEntry.set("materialname",material.getString("masterid.name"));//物料名称
         negainventoryOrderEntry.set("producttype",dynamicObject.getString("nckd_sideproduct"));//产品类型
         negainventoryOrderEntry.set("qualitystatus","A");//质量状态
         negainventoryOrderEntry.set("unit",dynamicObject.getDynamicObject("nckd_unitofmeasurement"));//计量单位
@@ -238,7 +239,7 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         invcountscheme.set("dept",dynamicObject.getDynamicObject("nckd_inventoryorg"));//库管部门
         invcountscheme.set("settlecurrency",1);//结算币别
         invcountscheme.set("bookdate",inventoryDynamicObject.getDate("nckd_inventoryclosedate"));//记账日期
-
+        invcountscheme.set("asyncstatus","B");//异步状态
         invcountscheme.set("creator", RequestContext.get().getCurrUserId());
         invcountscheme.set("createtime",new Date());
         invcountscheme.set("modifier", RequestContext.get().getCurrUserId());
@@ -252,6 +253,7 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         QFilter qFilter = new QFilter("masterid",QCP.equals,materieNumber);
         DynamicObject material = BusinessDataServiceHelper.loadSingle("bd_materialinventoryinfo", qFilter.toArray());
         negainventoryOrderEntry.set("material",material);//物料编码
+        negainventoryOrderEntry.set("materialname",material.getString("masterid.name"));//物料名称
         negainventoryOrderEntry.set("unit",dynamicObject.getDynamicObject("nckd_unitofmeasurement"));//计量单位
         negainventoryOrderEntry.set("qty",dynamicObject.getBigDecimal("nckd_number"));//数量
         negainventoryOrderEntry.set("baseunit",dynamicObject.getDynamicObject("nckd_basicunit"));//基本单位
@@ -286,10 +288,10 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         invcountscheme.set("biztype",biztype);//业务类型
         DynamicObject invscheme = BusinessDataServiceHelper.loadSingle(699728741555085312l, "im_invscheme");
         invcountscheme.set("invscheme",invscheme);//库存事务
-        Date date = inventoryDynamicObject.getDate("nckd_inventoryclosedate");
         invcountscheme.set("biztime",inventoryDynamicObject.getDate("nckd_inventoryclosedate"));//业务日期
         invcountscheme.set("billstatus","A");//单据状态
-        invcountscheme.set("bizorg",RequestContext.get().getOrgId());//业务组织
+        invcountscheme.set("asyncstatus","B");//异步状态
+        invcountscheme.set("bizorg",RequestContext.get().getOrgId());//需求组织
         invcountscheme.set("bizdept",dynamicObject.getDynamicObject("nckd_inventoryorg"));//业务部门
         invcountscheme.set("supplyownertype","bos_org");//货主类型
         invcountscheme.set("supplyowner",dynamicObject.getDynamicObject("nckd_inventoryorg"));//供应货主
@@ -315,6 +317,7 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         DynamicObject material = BusinessDataServiceHelper.loadSingle("bd_materialinventoryinfo", qFilter.toArray());
         DynamicObject materi = BusinessDataServiceHelper.loadSingle("bd_material", qFilter.toArray());
         negainventoryOrderEntry.set("material",material);//物料编码
+        negainventoryOrderEntry.set("materialname",material.getString("masterid.name"));//物料名称
         negainventoryOrderEntry.set("materialmasterid",materi);//主物料
         DynamicObject dynamicObject1 = dynamicObject.getDynamicObject("nckd_unitofmeasurement");
         negainventoryOrderEntry.set("unit",dynamicObject1);//计量单位
@@ -327,7 +330,12 @@ public class NegainventoryOrderListPlugin extends AbstractListPlugin {
         negainventoryOrderEntry.set("location",dynamicObject.getDynamicObject("nckd_position"));//仓位
         negainventoryOrderEntry.set("invtype",688884005529250816l);//入库库存类型
         negainventoryOrderEntry.set("invstatus",691928582720825344l);//入库库存状态
-        negainventoryOrderEntry.set("outinvtype",688884005529250816l);//出库库存状态
+        negainventoryOrderEntry.set("outinvtype",688884005529250816l);//出库库存类型
+        negainventoryOrderEntry.set("outinvstatus",691928582720825344l);//出库库存状态
+        negainventoryOrderEntry.set("outownertype","bos_org");//出库货主类型
+        negainventoryOrderEntry.set("outowner",dynamicObject.getDynamicObject("nckd_inventoryorg"));//出库货主
+        negainventoryOrderEntry.set("outkeepertype","bos_org");//出库保管者类型
+        negainventoryOrderEntry.set("outkeeper",dynamicObject.getDynamicObject("nckd_inventoryorg"));//出库保管者
         negainventoryOrderEntry.set("keepertype","bos_org");//入库保管者类型
 
         negainventoryOrderEntry.set("keeper",dynamicObject.getDynamicObject("nckd_inventoryorg"));//入库保管者
