0fecdaca7eff706601f2d141271c82f994b72400
wgq <839160351@qq.com>
2024-08-06 15:57:02 +0800

    调整插件

--- yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/OcbSaleOrderOperatePlugIn.java
+++ yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/OcbSaleOrderOperatePlugIn.java
@@ -12,10 +12,9 @@ import java.util.*;
 
 /**
  * 单据操作插件
- *  要货订单提交时,计算分摊金额
+ * 要货订单提交时,计算分摊金额
  */
-public class OcbSaleOrderOperatePlugIn extends AbstractOperationServicePlugIn  {
-
+public class OcbSaleOrderOperatePlugIn extends AbstractOperationServicePlugIn {
 
 
     /**
@@ -28,8 +27,9 @@ public class OcbSaleOrderOperatePlugIn extends AbstractOperationServicePlugIn  {
         e.getFieldKeys().add("resultseq");//促销结果组号
         e.getFieldKeys().add("orderentryid");//要货订单分录Id
         e.getFieldKeys().add("approveqty");//批准数量
-        e.getFieldKeys().add("nckd_amount");//测试金额
-        e.getFieldKeys().add("nckd_apportionamount");//分摊金额
+        e.getFieldKeys().add("standardamount");//标准金额
+        e.getFieldKeys().add("nckd_price");//分摊单价
+        //e.getFieldKeys().add("nckd_apportionamount");//分摊金额
         e.getFieldKeys().add("taxamount");//价税合计
     }
 
@@ -43,47 +43,52 @@ public class OcbSaleOrderOperatePlugIn extends AbstractOperationServicePlugIn  {
 
         DynamicObject[] entities = e.getDataEntities();
         // 逐单处理
-        for(DynamicObject dataEntity : entities){
+        for (DynamicObject dataEntity : entities) {
             //按促销明细的促销结果组号分组,获取商品明细的分录Id集合
-            Map<Integer, HashSet<Object>> orderEntryIdList=new HashMap<>();
+            Map<Integer, HashSet<Object>> orderEntryIdList = new HashMap<>();
             //促销明细单据体
-            DynamicObjectCollection promotion_entry=dataEntity.getDynamicObjectCollection("promotion_entry");
-            for(DynamicObject proRow:promotion_entry){
-                int resultSeq=proRow.getInt("resultseq");//促销结果组号
-                Object orderEntryId=proRow.get("orderentryid");//要货订单Id
-                if(!orderEntryIdList.containsKey(resultSeq)){
-                    orderEntryIdList.put(resultSeq,new HashSet<>());
+            DynamicObjectCollection promotion_entry = dataEntity.getDynamicObjectCollection("promotion_entry");
+            for (DynamicObject proRow : promotion_entry) {
+                int resultSeq = proRow.getInt("resultseq");//促销结果组号
+                Object orderEntryId = proRow.get("orderentryid");//要货订单Id
+                if (!orderEntryIdList.containsKey(resultSeq)) {
+                    orderEntryIdList.put(resultSeq, new HashSet<>());
                 }
                 orderEntryIdList.get(resultSeq).add(orderEntryId);
             }
-            if(!orderEntryIdList.isEmpty()){
+            if (!orderEntryIdList.isEmpty()) {
                 //商品明细单据体
-                DynamicObjectCollection itemEntry=dataEntity.getDynamicObjectCollection("itementry");
+                DynamicObjectCollection itemEntry = dataEntity.getDynamicObjectCollection("itementry");
                 //遍历分录Id集合
-                for(HashSet<Object>  entryIds:orderEntryIdList.values()){
-                    if(!entryIds.isEmpty()){
-                        BigDecimal taxAmountCount=BigDecimal.ZERO;//价税合计
-                        BigDecimal amountCount=BigDecimal.ZERO;//测试金额合计
-                        List<DynamicObject>objectList=new ArrayList<>();
-                        for(Object entryId:entryIds){
+                for (HashSet<Object> entryIds : orderEntryIdList.values()) {
+                    if (!entryIds.isEmpty()) {
+                        BigDecimal taxAmountCount = BigDecimal.ZERO;//价税合计
+                        BigDecimal amountCount = BigDecimal.ZERO;//测试金额合计
+                        List<DynamicObject> objectList = new ArrayList<>();
+                        for (Object entryId : entryIds) {
                             //商品明细分录
-                            DynamicObject entryRow=itemEntry.stream()
+                            DynamicObject entryRow = itemEntry.stream()
                                     .filter(dynamicObject -> entryId.equals(dynamicObject.getPkValue()))
                                     .findFirst()
                                     .orElse(null);
                             objectList.add(entryRow);
-                            BigDecimal taxAmount=entryRow.getBigDecimal("taxamount");
-                            taxAmountCount=taxAmountCount.add(taxAmount);
-                            BigDecimal amount=entryRow.getBigDecimal("nckd_amount");
-                            amountCount=amountCount.add(amount);
+                            BigDecimal taxAmount = entryRow.getBigDecimal("taxamount");
+                            taxAmountCount = taxAmountCount.add(taxAmount);
+                            BigDecimal amount = entryRow.getBigDecimal("standardamount");
+                            amountCount = amountCount.add(amount);
                         }
-                        if(!amountCount.equals(BigDecimal.ZERO)){
+                        if (!amountCount.equals(BigDecimal.ZERO)) {
                             //再次遍历计算分摊金额
-                            for(DynamicObject entryRow:objectList){
-                                BigDecimal amount=entryRow.getBigDecimal("nckd_amount");
-                                BigDecimal ratio=amount.divide(amountCount,10,BigDecimal.ROUND_HALF_UP);
-                                BigDecimal apportionAmount=taxAmountCount.multiply(ratio).setScale(6,BigDecimal.ROUND_HALF_UP);
-                                entryRow.set("nckd_apportionamount",apportionAmount);
+                            for (DynamicObject entryRow : objectList) {
+                                BigDecimal amount = entryRow.getBigDecimal("standardamount");
+                                BigDecimal qty = entryRow.getBigDecimal("approveqty");//批准数量
+                                BigDecimal ratio = amount.divide(amountCount, 10, BigDecimal.ROUND_HALF_UP);
+                                BigDecimal apportionAmount = taxAmountCount.multiply(ratio).setScale(6, BigDecimal.ROUND_HALF_UP);
+                                entryRow.set("nckd_apportionamount", apportionAmount);//分摊金额
+                                if (qty.compareTo(BigDecimal.ZERO) > 0) {
+                                    BigDecimal apportionPrice = apportionAmount.divide(qty, 6, BigDecimal.ROUND_HALF_UP);
+                                    entryRow.set("nckd_price", apportionPrice);//分摊单价
+                                }
                             }
                         }
 
