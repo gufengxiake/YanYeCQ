86da5d682b0e1d35a858fa1a1041296a246e1fc5
chengchaohua <xinhuhua@sohu.com>
2024-08-16 21:42:32 +0800

    岗位申请

---
+++ yanye/hr/nckd-yanye-hr-plugin/src/main/java/nckd/yanye/hr/plugin/form/web/position/PositionBillEditEx.java
@@ -0,0 +1,30 @@
+package nckd.yanye.hr.plugin.form.web.position;
+
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.logging.Log;
+import kd.bos.logging.LogFactory;
+import kd.hr.haos.business.servicehelper.OrgBatchBillHelper;
+import kd.hr.haos.common.constants.masterdata.AdminOrgConstants;
+import kd.hr.hbp.formplugin.web.HRCoreBaseBillEdit;
+
+import java.util.Date;
+import java.util.EventObject;
+
+/**
+ * 岗位申请单，标识：nckd_homs_positionb_ext
+ * author:chengchaohua
+ * date:2024-0816
+ */
+public class PositionBillEditEx extends HRCoreBaseBillEdit {
+
+    private static Log logger = LogFactory.getLog(PositionBillEditEx.class);
+
+    public void afterBindData(EventObject eventObject) {
+        // 行政组织
+        DynamicObject value = (DynamicObject)this.getModel().getValue("adminorg");
+        // 取行政组织的长名称
+        String orgLongName = OrgBatchBillHelper.getOrgLongName((Long)value.getPkValue(), new Date(), String.valueOf(AdminOrgConstants.ADMINORG_STRUCT));
+        this.getModel().setValue("nckd_orglongname", orgLongName);
+    }
+
+}
