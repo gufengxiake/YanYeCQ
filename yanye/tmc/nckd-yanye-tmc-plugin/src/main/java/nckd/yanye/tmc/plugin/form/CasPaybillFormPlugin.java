7e9b285f16d9d6e7517961bf6bed27671b007008
zhujintao <983068902@qq.com>
2024-08-16 10:41:14 +0800

    modify:调整提示语

--- yanye/tmc/nckd-yanye-tmc-plugin/src/main/java/nckd/yanye/tmc/plugin/form/CasPaybillFormPlugin.java
+++ yanye/tmc/nckd-yanye-tmc-plugin/src/main/java/nckd/yanye/tmc/plugin/form/CasPaybillFormPlugin.java
@@ -54,7 +54,8 @@ public class CasPaybillFormPlugin extends AbstractBillPlugIn {
                     if (!formOperate.getOption().tryGetVariableValue(OPPARAM_AFTERCONFIRM, afterConfirm)) {
                         // 显示确认消息
                         ConfirmCallBackListener confirmCallBacks = new ConfirmCallBackListener(KEY_BEFORESUBMIT, this);
-                        this.getView().showConfirm("供应商为" + unittypeMap.get(unittype) + "的单位，是否继续提交?", MessageBoxOptions.YesNo, ConfirmTypes.Default, confirmCallBacks);
+                        //收款单位为失信单位，是否继续付款
+                        this.getView().showConfirm("收款单位为" + unittypeMap.get(unittype) + "，是否继续付款?", MessageBoxOptions.YesNo, ConfirmTypes.Default, confirmCallBacks);
                         // 在没有确认之前，先取消本次操作
                         args.setCancel(true);
                     }
