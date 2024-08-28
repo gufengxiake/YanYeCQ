64f0825f2d1388d47c98d18a51ff396b831d36de
yaosijie <1783630922@qq.com>
2024-08-28 16:34:27 +0800

    5G工厂配置类修改

--- yanye/scm/nckd-yanye-scm-common/src/main/java/nckd/yanye/scm/common/utils/FactoryUtil.java
+++ yanye/scm/nckd-yanye-scm-common/src/main/java/nckd/yanye/scm/common/utils/FactoryUtil.java
@@ -1,4 +1,7 @@
 package nckd.yanye.scm.common.utils;
+
+import nckd.base.common.utils.capp.CappConfig;
+
 /**
  * Module           :
  * Description      : 5G工厂接口配置类
@@ -16,28 +19,14 @@ public class FactoryUtil {
      * lkIjoxMDAwNCwidGVuYW50SWQiOiIiLCJjbGllbnRGbGFnIjoiIiwidXNlclR5cGUiOiJCdXNpbmVzc
      * 1NlcnZpY2UiLCJpYXQiOjE3MjQzNzY3MTg1Nzd9.xiTewvVO8GiA9ba9r6GAr8Odq9vI7iHxt_oqTYgPsqo
      */
-    public static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJs" +
-            "b2dpbl90b2tlbiIsImV4cFRpbWUiOjMzMDIyMTM1MTg1NzUsImlzcyI6ImN5YnN0YXIiLCJsb2dpbkRhd" +
-            "GUiOjE3MjQzNzY3MTg1NzUsInJlcXVlc3RJcCI6IiIsInVzZXJOYW1lIjoi6YeR6J225LqR5pif54CaLem" +
-            "Hkeidtui9r-S7tiIsInVzZXJJZCI6MjksInVzZXJDb2RlIjoi6YeR6J226L2v5Lu2IiwidGVuYW50TmFtZ" +
-            "SI6IiIsImFwcElkIjoxMDAwNCwidGVuYW50SWQiOiIiLCJjbGllbnRGbGFnIjoiIiwidXNlclR5cGUiOiJ" +
-            "CdXNpbmVzc1NlcnZpY2UiLCJpYXQiOjE3MjQzNzY3MTg1Nzd9.xiTewvVO8GiA9ba9r6GAr8Odq9vI7iHxt_oqTYgPsqo";
-    /**
-     * 5G工厂userCode
-     */
-    public static final String USERCODE = "金蝶软件";
-    /**
-     * 5G工厂AppId
-     */
-    public static final String APPID = "10004";
-
+    public static final String TOKEN = CappConfig.getConfigValue("5G_token");
     /**
      * 5G工厂接口（外网）
      */
-    public static final String INTRANETFACTORYUURL = "http://106.225.128.131:65500/mes/api/Index/IndexData/QueryByName";
+    public static final String INTRANETFACTORYUURL = CappConfig.getConfigValue("5G_externalurl");
 
     /**
      * 5G工厂接口（内网）
      */
-    public static final String EXTERNALNETFACTORYUURL = "http://http://10.66.2.207/mes/api/Index/IndexData/QueryByName";
+    public static final String EXTERNALNETFACTORYUURL = CappConfig.getConfigValue("5G_intraneturl");
 }
