653b1e673c4c2069062a594161f2fa9b24747872
guozhiwei <2907499219@qq.com>
2024-09-02 11:07:00 +0800

    为他人申请单据功能修改

--- yanye/hr/nckd-yanye-hr-plugin/src/main/java/nckd/yanye/hr/plugin/form/kaoqin/ApplyBussinessShiftValidator.java
+++ yanye/hr/nckd-yanye-hr-plugin/src/main/java/nckd/yanye/hr/plugin/form/kaoqin/ApplyBussinessShiftValidator.java
@@ -10,30 +10,33 @@ import kd.bos.entity.plugin.AddValidatorsEventArgs;
 import kd.bos.entity.plugin.PreparePropertysEventArgs;
 import kd.bos.entity.validate.AbstractValidator;
 import kd.bos.servicehelper.MetadataServiceHelper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 
 /**
- * Module           :工时假勤云-日常考勤-为他人申请补签校验插件
- * Description      :为他人申请补签校验插件
+ * Module           :工时假勤云-假期管理-为他人申请调班校验插件
+ * Description      :为他人申请调班校验插件
  *
  * @author guozhiwei
- * @date  2024-08-30 15：09
+ * @date  2024-08-30 14:59
+ * wtabm_vaapplymob
  *
- *   wtpm_supsignpc
  *
  */
 
-public class ApplyBussinessReplaceValidator extends AbstractOperationServicePlugIn {
+public class ApplyBussinessShiftValidator extends AbstractOperationServicePlugIn {
 
     @Override
     public void onPreparePropertys(PreparePropertysEventArgs e) {
         super.onPreparePropertys(e);
         // 提前加载表单里的字段
         List<String> fieldKeys = e.getFieldKeys();
-        MainEntityType dt = MetadataServiceHelper.getDataEntityType("wtpm_supsignpc");
+        MainEntityType dt = MetadataServiceHelper.getDataEntityType("wts_swshiftbill");
         Map<String, IDataEntityProperty> fields = dt.getAllFields();
         fields.forEach((Key, value) -> {
             fieldKeys.add(Key);
@@ -52,17 +55,15 @@ public class ApplyBussinessReplaceValidator extends AbstractOperationServicePlug
                 ExtendedDataEntity[] dataEntities = this.getDataEntities();
                 for (ExtendedDataEntity dataEntity : dataEntities) {
                     DynamicObject dataEntityObj = dataEntity.getDataEntity();
-                    // 获取
-                    Date createtime = dataEntityObj.getDate("createtime");
+
                     // 获取本周一的日期
                     Date monday = ApplyBussinessWorkValidator.getMonday();
                     boolean flag = false;
                     // 获取分录开始时间，判断是否在本周内
                     DynamicObjectCollection entryentity = dataEntityObj.getDynamicObjectCollection("entryentity");
                     for (DynamicObject dynamicObject : entryentity) {
-                        // 获取补签日期
-                        Date entrytime = dynamicObject.getDate("signdate");
-                        if(entrytime.before(monday)){
+                        // 获取调班日期
+                        if(dynamicObject.getDate("swdate").before(monday)){
                             flag = true;
                             break;
                         }
