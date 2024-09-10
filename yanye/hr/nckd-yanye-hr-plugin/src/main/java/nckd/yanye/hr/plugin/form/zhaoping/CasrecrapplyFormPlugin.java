38d1f24b7bf18dc697a803b1386652cbc330e823
guozhiwei <2907499219@qq.com>
2024-09-10 18:07:53 +0800

    编制调整申请修改，临时招聘申请单据插件

---
+++ yanye/hr/nckd-yanye-hr-plugin/src/main/java/nckd/yanye/hr/plugin/form/zhaoping/CasrecrapplyFormPlugin.java
@@ -0,0 +1,182 @@
+package nckd.yanye.hr.plugin.form.zhaoping;
+
+
+import kd.bos.bill.AbstractBillPlugIn;
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.entity.datamodel.events.ChangeData;
+import kd.bos.entity.datamodel.events.PropertyChangedArgs;
+import kd.bos.form.control.EntryGrid;
+import kd.bos.form.field.BasedataEdit;
+import kd.bos.form.field.events.BeforeF7SelectEvent;
+import kd.bos.form.field.events.BeforeF7SelectListener;
+import kd.bos.list.ListShowParameter;
+import kd.bos.orm.query.QCP;
+import kd.bos.orm.query.QFilter;
+import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.bos.servicehelper.DispatchServiceHelper;
+import org.apache.commons.lang3.ObjectUtils;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Module           :人才供应云-招聘直通车-首页-临时招聘申请
+ * Description      :临时招聘申请单据插件
+ *
+ * @author guozhiwei
+ * @date  2024/9/10 9：40
+ * 标识 nckd_casrecrapply
+ */
+
+
+
+public class CasrecrapplyFormPlugin extends AbstractBillPlugIn implements BeforeF7SelectListener {
+
+    // 公司类型
+    private final List<String> COMPANY_LIST = Arrays.asList(new String[]{"1020_S","1050_S","1060_S","1070_S"});
+
+    // 定义日期格式
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+
+    @Override
+    public void afterCreateNewData(EventObject e) {
+        super.afterCreateNewData(e);
+        DynamicObject org = (DynamicObject) this.getModel().getValue("org");
+        //  组织id
+        Long pkValue = (Long) org.getPkValue();
+        List<Long> longs = new ArrayList<Long>();
+        longs.add(pkValue);
+        Object[] objects = new Object[2];
+
+        // 设置数组元素
+        objects[0] = new Date();
+        objects[1] = longs;
+
+        DispatchServiceHelper.invokeService("kd.hrmp.haos.servicehelper","haos","IHAOSStaffService","queryUseStaffInfo",objects);
+
+    }
+
+    @Override
+    public void propertyChanged(PropertyChangedArgs e) {
+        super.propertyChanged(e);
+        String key = e.getProperty().getName();
+        ChangeData[] changeData = e.getChangeSet();
+        Object newValue = changeData[0].getNewValue();
+        Object oldValue = changeData[0].getOldValue();
+        int iRow = changeData[0].getRowIndex();
+        switch (key) {
+            case "nckd_recruitcompany":
+                // 公司为空清除部门岗位
+                if(!isNotEmpty(newValue)){
+                    this.getModel().setValue("nckd_recruitorg", null,iRow);
+                    this.getModel().setValue("nckd_recruitpost", null,iRow);
+                }
+                break;
+            case "nckd_recruitorg":
+                // 部门，判断是否存企业，如果不存在企业则将部门的所属公司带入到企业中
+                if(isNotEmpty(newValue)){
+                    DynamicObject newValue1 = (DynamicObject) newValue;
+                    this.getModel().setValue("nckd_recruitcompany", newValue1.getDynamicObject("belongcompany").getPkValue(),iRow);
+                }else{
+                    this.getModel().setValue("nckd_recruitpost", null,iRow);
+                }
+                break;
+            case "nckd_recruitpost":
+                // 岗位，判断是否存企业，如果不存在企业则将部门的所属公司带入到企业中
+                if(isNotEmpty(newValue)){
+                    DynamicObject newValue1 = (DynamicObject) newValue;
+                    // 获取行政组织，和行政组织的所属公司
+                    // 行政组织
+                    DynamicObject adminorg = newValue1.getDynamicObject("adminorg");
+                    DynamicObject dynamicObject = BusinessDataServiceHelper.loadSingle(adminorg.getPkValue(), "haos_adminorgf7");
+                    this.getModel().setValue("nckd_recruitorg", dynamicObject.getDynamicObject("belongcompany").getPkValue(),iRow);
+
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    // 公司，部门，岗位选择过滤
+    @Override
+    public void beforeF7Select(BeforeF7SelectEvent e) {
+        String fieldKey = e.getProperty().getName();
+        switch (fieldKey){
+            case "nckd_recruitcompany":
+                // 企业名称
+                ListShowParameter showParameter = (ListShowParameter)e.getFormShowParameter();
+                //是否展示审核的改为false
+                QFilter qFilter = new QFilter("adminorgtype.number", "in", COMPANY_LIST);
+                showParameter.getListFilterParameter().setFilter(qFilter);
+
+                break;
+            case "nckd_recruitorg":
+                // 组织名称
+                ListShowParameter showParameter2 = (ListShowParameter)e.getFormShowParameter();
+                // 展示部门，如果选择了企业，展示企业下的部门
+                QFilter qFilter2 = new QFilter("adminorgtype.number", QCP.in, "1040_S");
+                EntryGrid treeEntryEntity = this.getControl("entryentity");
+                int[] rows = treeEntryEntity.getSelectRows();
+                if(rows.length > 0){
+                    // 企业不为null，添加企业筛选条件
+                    DynamicObject nckdRecruitcompany = (DynamicObject)this.getModel().getValue("nckd_recruitcompany", rows[0]);
+                    if(ObjectUtils.isNotEmpty(nckdRecruitcompany)){
+                        qFilter2.and("belongcompany.number", QCP.like, nckdRecruitcompany.getString("number")+"%");
+                    }
+                }
+                showParameter2.getListFilterParameter().setFilter(qFilter2);
+                break;
+            case "nckd_recruitpost":
+                // 岗位名称
+                ListShowParameter showParameter3 = (ListShowParameter)e.getFormShowParameter();
+                // 展示部门，如果选择了企业，展示企业下的部门
+                EntryGrid treeEntryEntity2 = this.getControl("entryentity");
+                int[] rows2 = treeEntryEntity2.getSelectRows();
+                DynamicObject nckdRecruitcompany2 = (DynamicObject)this.getModel().getValue("nckd_recruitcompany", rows2[0]);
+                DynamicObject nckdRecruitorg = (DynamicObject)this.getModel().getValue("nckd_recruitorg", rows2[0]);
+                QFilter qFilter1 = null;
+                if(ObjectUtils.isNotEmpty(nckdRecruitcompany2)){
+                    qFilter1 = new QFilter("adminorg.number", QCP.like, nckdRecruitcompany2.getString("number") + "%");
+                }
+                if(ObjectUtils.isNotEmpty(nckdRecruitorg)){
+                    if(qFilter1 ==null){
+                        qFilter1 = new QFilter("adminorg.id", QCP.equals, nckdRecruitorg.getPkValue());
+                    }else{
+                        qFilter1.and("adminorg.id", QCP.equals, nckdRecruitorg.getPkValue());
+                    }
+                }
+                showParameter3.getListFilterParameter().setFilter(qFilter1);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+    @Override
+    public void registerListener(EventObject e) {
+        BasedataEdit fieldEdit = (BasedataEdit) this.getView().getControl("nckd_recruitcompany");
+        BasedataEdit fieldEdit2 = (BasedataEdit) this.getView().getControl("nckd_recruitorg");
+        BasedataEdit fieldEdit3 = (BasedataEdit) this.getView().getControl("nckd_recruitpost");
+        fieldEdit.addBeforeF7SelectListener(this);
+        fieldEdit.addBeforeF7SelectListener(this);
+        fieldEdit2.addBeforeF7SelectListener(this);
+        fieldEdit3.addBeforeF7SelectListener(this);
+    }
+
+
+    public static boolean isNotEmpty(Object  key) {
+        // 基础资料判空
+        if(ObjectUtils.isEmpty(key) || ObjectUtils.isEmpty(((DynamicObject)key).getDataStorage())){
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
