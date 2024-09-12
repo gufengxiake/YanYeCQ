f144c58cb4f25fb8ccf049c9bec40253211ca376
zhujintao <983068902@qq.com>
2024-09-12 14:47:31 +0800

    modfiy:认领处理移动端界面增加要货订单选择列表

--- yanye/tmc/nckd-yanye-tmc-plugin/src/main/java/nckd/yanye/tmc/plugin/mobile/RecClaimBillDetailMobBillExtPlugin.java
+++ yanye/tmc/nckd-yanye-tmc-plugin/src/main/java/nckd/yanye/tmc/plugin/mobile/RecClaimBillDetailMobBillExtPlugin.java
@@ -1,28 +1,31 @@
 package nckd.yanye.tmc.plugin.mobile;
 
 import kd.bos.bill.AbstractMobBillPlugIn;
-import kd.bos.bill.MobileBillShowParameter;
-import kd.bos.bill.MobileFormPosition;
-import kd.bos.bill.OperationStatus;
 import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.dataentity.metadata.IDataEntityProperty;
 import kd.bos.dataentity.resource.ResManager;
 import kd.bos.dataentity.utils.StringUtils;
+import kd.bos.entity.datamodel.IDataModel;
 import kd.bos.entity.datamodel.ListSelectedRow;
 import kd.bos.entity.datamodel.ListSelectedRowCollection;
+import kd.bos.entity.datamodel.events.PropertyChangedArgs;
 import kd.bos.form.CloseCallBack;
-import kd.bos.form.FormConfig;
-import kd.bos.form.FormMetadataCache;
+import kd.bos.form.IFormView;
 import kd.bos.form.ShowType;
 import kd.bos.form.control.Control;
 import kd.bos.form.events.ClosedCallBackEvent;
 import kd.bos.list.ListFilterParameter;
 import kd.bos.list.MobileListShowParameter;
-import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
+import kd.bos.orm.util.CollectionUtils;
+import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.bos.servicehelper.QueryServiceHelper;
 import kd.fi.cas.formplugin.mobile.recclaim.utils.CasToolKit;
+import kd.fi.cas.formplugin.mobile.recclaim.utils.EmptyUtil;
 
 import java.util.EventObject;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Module           :财务云-出纳-收款认领
@@ -32,7 +35,6 @@ import java.util.Iterator;
  * @date : 2024/9/9
  */
 public class RecClaimBillDetailMobBillExtPlugin extends AbstractMobBillPlugIn {
-
     @Override
     public void registerListener(EventObject e) {
         super.registerListener(e);
@@ -46,101 +48,131 @@ public class RecClaimBillDetailMobBillExtPlugin extends AbstractMobBillPlugIn {
         String key = control.getKey();
         if ("corebillnobtn".equals(key)) {
             this.showCoreBillF7();
-        } else {
+        } /*else {
             if ("corebillentrybtn".equals(key)) {
                 this.showCoreBillEntry();
             }
-        }
+        }*/
     }
 
-    private void showCoreBillF7() {
-        String coreBillType = (String) this.getModel().getValue("e_corebilltype");
-        if ("ocbsoc_saleorder".equals(coreBillType)) {
-            Object settleorg = this.getModel().getValue("e_settleorg");
-            ListFilterParameter lfp = new ListFilterParameter();
-            lfp.setFilter(new QFilter("saleorgid", QCP.equals, settleorg != null ? ((DynamicObject) settleorg).getPkValue() : null));
-            //MobileListShowParameter lsp = new MobileListShowParameter();
-            MobileListShowParameter lsp = createShowMobileF7ListForm("nckd_ocbsoc_saleorderbase", false);//第二个参数为是否支持多选;
-            CloseCallBack closeCallBack = new CloseCallBack(this, "e_corebillno_ext");
-            //lsp.setFormId("bos_moblisttabf7");
-            //lsp.setBillFormId("nckd_ocbsoc_saleorderbase");
-            //lsp.setCustomParam("ismergerows", Boolean.FALSE);
-            //lsp.setCaption(ResManager.loadKDString("核心单据编号", "RecClaimBillDetailMobBillPlugin_3", "fi-cas-mobile", new Object[0]));
-            //lsp.getOpenStyle().setShowType(ShowType.Floating);
-            //lsp.setLookUp(true);
-            //lsp.setListFilterParameter(lfp);
-            lsp.setCloseCallBack(closeCallBack);
-            this.getView().showForm(lsp);
+    /*@Override
+    public void propertyChanged(PropertyChangedArgs e) {
+        super.propertyChanged(e);
+        IDataModel model = this.getModel();
+        IFormView view = this.getView();
+        IDataEntityProperty iDataEntityProperty = e.getProperty();
+        String propertyName = iDataEntityProperty.getName();
+        Object newValueObj;
+        Object coreBillTypeObj = this.getModel().getValue("e_corebilltype");
+        if (EmptyUtil.isEmpty(coreBillTypeObj)) {
+            model.beginInit();
+            model.setValue("e_corebillno", (Object) null);
+            model.setValue("e_corebillid", (Object) null);
+            model.endInit();
+            view.showTipNotification(ResManager.loadKDString("请先指定核心单据类型。", "RecClaimBillDetailMobBillPlugin_0", "fi-cas-mobile", new Object[0]));
+            return;
         }
-    }
-
-    /*
-    创建移动列表
-     */
-    private static MobileListShowParameter createShowMobileF7ListForm(String formId, boolean isMultiSelect) {
-        MobileListShowParameter para = new MobileListShowParameter();
-        FormConfig formConfig = FormMetadataCache.getMobListFormConfig(formId);
-        para.setCaption(formConfig.getCaption().toString());
-        para.setLookUp(true);
-        para.setBillFormId(formId);
-        ShowType showType;
-        if (formConfig.getShowType() == ShowType.MainNewTabPage) {
-            showType = ShowType.Floating;
-        } else {
-            showType = ShowType.Modal;
-            para.setPosition(MobileFormPosition.Bottom);
-        }
-        para.getOpenStyle().setShowType(showType);
-        para.setMultiSelect(isMultiSelect);
-
-        String f7ListFormId = formConfig.getF7ListFormId();
-        if (StringUtils.isNotBlank(f7ListFormId)) {
-            para.setFormId(f7ListFormId);
-        }
-
-        return para;
-    }
-
-    private void showCoreBillEntry() {
-        String coreBillType = (String) this.getModel().getValue("e_corebilltype");
-        if ("ocbsoc_saleorder".equals(coreBillType)) {
-            String coreBillId = (String) this.getModel().getValue("e_corebillid");
-            if (coreBillId != null && !StringUtils.isEmpty(coreBillId)) {
-                Object settleOrgId = ((DynamicObject) this.getModel().getValue("e_settleorg")).getPkValue();
-                if (settleOrgId == null) {
-                    this.getView().showTipNotification(ResManager.loadKDString("请先指定结算组织。", "RecClaimBillDetailMobBillPlugin_1", "fi-cas-mobile", new Object[0]));
-                } else {
-                    MobileBillShowParameter bsp = new MobileBillShowParameter();
-                    CloseCallBack closeCallBack = new CloseCallBack(this, "e_corebillentry");
-                    bsp.setFormId("ocbsoc_saleorder");
-                    bsp.setPkId(coreBillId);
-                    bsp.setCaption(ResManager.loadKDString("核心单据行号", "RecClaimBillDetailMobBillPlugin_6", "fi-cas-mobile", new Object[0]));
-                    bsp.setStatus(OperationStatus.VIEW);
-                    bsp.setCustomParam("org", settleOrgId);
-                    bsp.getOpenStyle().setShowType(ShowType.Floating);
-                    bsp.setCloseCallBack(closeCallBack);
-                    this.getView().showForm(bsp);
+        String coreBillType = (String) coreBillTypeObj;
+        if (StringUtils.equals(propertyName, "e_corebillno") && "ocbsoc_saleorder".equals(coreBillType)) {
+            model.setValue("e_corebillid", (Object) null);
+            model.setValue("e_corebillentryseq", (Object) null);
+            model.setValue("e_corebillentryid", (Object) null);
+            newValueObj = e.getChangeSet()[0].getNewValue();
+            if (EmptyUtil.isNoEmpty(newValueObj)) {
+                String coreBillNo = (String) newValueObj;
+                if ("ocbsoc_saleorder".equals(coreBillType)) {
+                    List<Object> coreBillCollIds = QueryServiceHelper.queryPrimaryKeys(coreBillType, new QFilter[]{new QFilter("billno", "=", coreBillNo)}, (String) null, 1);
+                    if (!CollectionUtils.isEmpty(coreBillCollIds)) {
+                        model.setValue("e_corebillid", coreBillCollIds.get(0));
+                    } else {
+                        model.beginInit();
+                        model.setValue("e_corebillno", (Object) null);
+                        model.setValue("e_corebillid", (Object) null);
+                        model.endInit();
+                        view.showTipNotification(ResManager.loadKDString("对应核心单据类型不存在此核心单据编号。", "RecClaimBillDetailMobBillPlugin_2", "fi-cas-mobile", new Object[0]));
+                    }
                 }
-            } else {
-                this.getView().showTipNotification(ResManager.loadKDString("请先指定核心单据编号。", "RecClaimBillDetailMobBillPlugin_5", "fi-cas-mobile", new Object[0]));
             }
 
         }
+    }*/
 
-    }
-
+    @Override
     public void closedCallBack(ClosedCallBackEvent evt) {
         super.closedCallBack(evt);
         String actionId = evt.getActionId();
-        if ("e_corebillno".equals(actionId)) {
+        if ("e_corebillno_ext".equals(actionId)) {
             this.setCoreBillNo(evt);
-        } else {
+        } /*else {
             if ("e_corebillentry".equals(actionId)) {
                 this.setCoreBillEntry(evt);
             }
 
+        }*/
+    }
+
+    private void showCoreBillF7() {
+        String coreBillType = (String) this.getModel().getValue("e_corebilltype");
+
+        if ("ocbsoc_saleorder".equals(coreBillType)) {
+            DynamicObject settleOrgDy = (DynamicObject) this.getModel().getValue("e_settleorg");
+            Object settleOrgId = settleOrgDy != null ? settleOrgDy.getPkValue() : null;
+            if (settleOrgId == null) {
+                this.getView().showTipNotification(ResManager.loadKDString("请先指定结算组织。", "RecClaimBillDetailMobBillPlugin_1", "fi-cas-mobile", new Object[0]));
+            } else {
+                ListFilterParameter lfp = new ListFilterParameter();
+
+                lfp.setFilter(new QFilter("saleorgid", "=", settleOrgId));
+                //lfp.setFilter(new QFilter("billstatus", "=", "C"));
+
+                MobileListShowParameter lsp = new MobileListShowParameter();
+                CloseCallBack closeCallBack = new CloseCallBack(this, "e_corebillno_ext");
+                lsp.setFormId("bos_moblisttabf7");
+                lsp.setBillFormId("nckd_ocbsoc_saleorderbase");
+                lsp.setCustomParam("ismergerows", Boolean.FALSE);
+                lsp.setCaption(ResManager.loadKDString("核心单据编号", "RecClaimBillDetailMobBillPlugin_3", "fi-cas-mobile", new Object[0]));
+                lsp.getOpenStyle().setShowType(ShowType.Floating);
+                lsp.setLookUp(true);
+                lsp.setListFilterParameter(lfp);
+                lsp.setCloseCallBack(closeCallBack);
+                this.getView().showForm(lsp);
+            }
         }
     }
+
+    /*private void showCoreBillEntry() {
+        String coreBillType = (String) this.getModel().getValue("e_corebilltype");
+        if (StringUtils.isEmpty(coreBillType)) {
+            this.getView().showTipNotification(ResManager.loadKDString("请先指定核心单据类型。", "RecClaimBillDetailMobBillPlugin_0", "fi-cas-mobile", new Object[0]));
+        } else {
+            ClaimCoreBillTypeEnum coreBillTypeEnum = ClaimCoreBillTypeEnum.getEnum(coreBillType);
+            if (coreBillTypeEnum != null) {
+                if (coreBillTypeEnum == ClaimCoreBillTypeEnum.SALORDER || coreBillTypeEnum == ClaimCoreBillTypeEnum.SALCONTRACT) {
+                    String coreBillId = (String) this.getModel().getValue("e_corebillid");
+                    if (coreBillId != null && !StringUtils.isEmpty(coreBillId)) {
+                        Object settleOrgId = ((DynamicObject) this.getModel().getValue("e_settleorg")).getPkValue();
+                        if (settleOrgId == null) {
+                            this.getView().showTipNotification(ResManager.loadKDString("请先指定结算组织。", "RecClaimBillDetailMobBillPlugin_1", "fi-cas-mobile", new Object[0]));
+                        } else {
+                            String formId = coreBillTypeEnum.getMobCode() == null ? coreBillTypeEnum.getCode() : coreBillTypeEnum.getMobCode();
+                            MobileBillShowParameter bsp = new MobileBillShowParameter();
+                            CloseCallBack closeCallBack = new CloseCallBack(this, "e_corebillentry");
+                            bsp.setFormId(formId);
+                            bsp.setPkId(coreBillId);
+                            bsp.setCaption(ResManager.loadKDString("核心单据行号", "RecClaimBillDetailMobBillPlugin_6", "fi-cas-mobile", new Object[0]));
+                            bsp.setStatus(OperationStatus.VIEW);
+                            bsp.setCustomParam("org", settleOrgId);
+                            bsp.getOpenStyle().setShowType(ShowType.Floating);
+                            bsp.setCloseCallBack(closeCallBack);
+                            this.getView().showForm(bsp);
+                        }
+                    } else {
+                        this.getView().showTipNotification(ResManager.loadKDString("请先指定核心单据编号。", "RecClaimBillDetailMobBillPlugin_5", "fi-cas-mobile", new Object[0]));
+                    }
+                }
+            }
+        }
+    }*/
 
     private void setCoreBillNo(ClosedCallBackEvent evt) {
         Object returnData = evt.getReturnData();
@@ -153,19 +185,22 @@ public class RecClaimBillDetailMobBillExtPlugin extends AbstractMobBillPlugIn {
 
                 while (var4.hasNext()) {
                     ListSelectedRow listSelectedRow = (ListSelectedRow) var4.next();
-                    this.getModel().setValue("e_corebillno", listSelectedRow.getBillNo());
+                    Object primaryKeyValue = listSelectedRow.getPrimaryKeyValue();
+                    DynamicObject ocbsocSaleorder = BusinessDataServiceHelper.loadSingle(primaryKeyValue, "ocbsoc_saleorder");
+                    this.getModel().setValue("e_corebillno", ocbsocSaleorder.getString("billno"));
+                    this.getModel().setValue("e_corebillid", ocbsocSaleorder.getPkValue());
                 }
 
             }
         }
     }
 
-    private void setCoreBillEntry(ClosedCallBackEvent evt) {
+    /*private void setCoreBillEntry(ClosedCallBackEvent evt) {
         Object returnData = evt.getReturnData();
         if (!CasToolKit.isEmpty(returnData)) {
             DynamicObject returnDataDy = (DynamicObject) returnData;
             this.getModel().setValue("e_corebillentryseq", returnDataDy.get("entryseq"));
         }
-    }
+    }*/
 
 }
