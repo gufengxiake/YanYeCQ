f6b544c47f86f564aa079447cc2bce3923874b1a
Zhangzl98 <2437592057@qq.com>
2024-09-25 19:12:34 +0800

    新增销售出库到财务应付转换插件

--- yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/SalOutSaveOperationPlugIn.java
+++ yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/SalOutSaveOperationPlugIn.java
@@ -14,7 +14,10 @@ import java.math.BigDecimal;
 import java.util.*;
 
 /**
- * 单据操作插件
+ * 销售出库保存更新配送费单价-单据操作插件
+ * 表单标识：nckd_im_saloutbill_ext
+ * author:zhangzhilong
+ * date:2024/09/25
  */
 public class SalOutSaveOperationPlugIn extends AbstractOperationServicePlugIn implements Plugin {
 
@@ -34,30 +37,32 @@ public class SalOutSaveOperationPlugIn extends AbstractOperationServicePlugIn im
 
         DynamicObject[] dataEntities = e.getDataEntities();
         for (DynamicObject dataEntity : dataEntities) {
-            Long bizorg = dataEntity.getLong("bizorg");
-            Long deliverchannel = dataEntity.getLong("nckd_deliverchannel");
+            Long bizorg = (Long) dataEntity.getDynamicObject("bizorg").getPkValue();
+            Long deliverchannel = (Long) dataEntity.getDynamicObject("nckd_deliverchannel").getPkValue();
             Date biztime = dataEntity.getDate("biztime");
             Long supplierId = this.getSupplierId(deliverchannel);
             BigDecimal purContractPrice = this.getPurContractPrice(bizorg, supplierId,biztime);
-            //nckd_pricefielpsf
-            DynamicObjectCollection billentry = dataEntity.getDynamicObjectCollection("billentry");
-            for (DynamicObject entryRow : billentry) {
-                entryRow.set("nckd_pricefielpsf",purContractPrice);
+            if(purContractPrice.compareTo(BigDecimal.ZERO) > 0){
+                DynamicObjectCollection billentry = dataEntity.getDynamicObjectCollection("billentry");
+                for (DynamicObject entryRow : billentry) {
+                    entryRow.set("nckd_pricefielpsf",purContractPrice);
+                }
             }
+
         }
 
     }
     public BigDecimal getPurContractPrice(Long org, Long supplierId,Date biztime) {
-        QFilter qFilter = new QFilter("validstatus",QCP.equals,"B")
-                .and("org", QCP.equals, org)
-                .and("supplier", QCP.equals, supplierId)//供应商
+        QFilter qFilter = new QFilter("org", QCP.equals, org)
+                .and("supplier", QCP.equals, supplierId)//供应商;
+                .and("validstatus",QCP.equals,"B")
                 .and("billstatus", QCP.equals, "C")
                 .and("closestatus", QCP.equals, "A")
                 .and("biztimeend",QCP.large_equals,biztime)
                 .and("biztimebegin",QCP.less_equals,biztime);
         DynamicObjectCollection conm_purcontract = QueryServiceHelper.query("conm_purcontract", "billentry.priceandtax as priceandtax,org,supplier"
                 , new QFilter[]{qFilter}, "biztimebegin desc");
-        if (conm_purcontract == null){
+        if (conm_purcontract == null || conm_purcontract.isEmpty()){
             return BigDecimal.ZERO;
         }
         return conm_purcontract.get(0).getBigDecimal("priceandtax");
@@ -77,12 +82,12 @@ public class SalOutSaveOperationPlugIn extends AbstractOperationServicePlugIn im
             return 0L;
         }
         long bizpartner = bd_customer.getLong("bizpartner");
-        DynamicObject bd_bizpartner = QueryServiceHelper.queryOne("bd_bizpartner", "entryentity_supplier.supid as supplierId",
-                new QFilter[]{new QFilter("id", QCP.equals, bizpartner)});
+        DynamicObject bd_bizpartner = QueryServiceHelper.queryOne("bd_supplier", "id",
+                new QFilter[]{new QFilter("bizpartner", QCP.equals, bizpartner)});
         if (bd_bizpartner == null){
             return 0L;
         }
-        return bd_bizpartner.getLong("supplierId");
+        return bd_bizpartner.getLong("id");
 
     }
 }
