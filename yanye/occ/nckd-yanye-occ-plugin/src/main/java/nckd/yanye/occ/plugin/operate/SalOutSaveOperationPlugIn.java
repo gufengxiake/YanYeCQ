1be2dfd8311ebe159b9d1df4fadb74ca1e4a34c2
wgq <839160351@qq.com>
2024-09-26 12:44:13 +0800

    要货订单新增配送员

--- yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/SalOutSaveOperationPlugIn.java
+++ yanye/occ/nckd-yanye-occ-plugin/src/main/java/nckd/yanye/occ/plugin/operate/SalOutSaveOperationPlugIn.java
@@ -4,10 +4,12 @@ import kd.bos.dataentity.entity.DynamicObject;
 import kd.bos.dataentity.entity.DynamicObjectCollection;
 import kd.bos.entity.plugin.AbstractOperationServicePlugIn;
 import kd.bos.entity.plugin.PreparePropertysEventArgs;
+import kd.bos.entity.plugin.args.BeforeOperationArgs;
 import kd.bos.entity.plugin.args.BeginOperationTransactionArgs;
 import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.QueryServiceHelper;
+import kd.bos.servicehelper.operation.SaveServiceHelper;
 import kd.sdk.plugin.Plugin;
 
 import java.math.BigDecimal;
@@ -28,41 +30,63 @@ public class SalOutSaveOperationPlugIn extends AbstractOperationServicePlugIn im
         e.getFieldKeys().add("nckd_deliverchannel");//配送渠道
         e.getFieldKeys().add("biztime");//业务日期
         e.getFieldKeys().add("billentry.nckd_pricefielpsf");//配送费单价
+        e.getFieldKeys().add("billentry.qty");//数量
+        e.getFieldKeys().add("billentry.nckd_pricefield3");//标准基价
+        e.getFieldKeys().add("billentry.amountandtax");//价税合计
+        e.getFieldKeys().add("billentry.nckd_remainpayamount");//剩余应付金额
+        e.getFieldKeys().add("billentry.nckd_premiumamount");//溢价金额
+        e.getFieldKeys().add("billentry.nckd_pricefielpsf");//配送费单价
+        e.getFieldKeys().add("billentry.nckd_basicqtyps");//配送结算基本数量
     }
 
-
     @Override
-    public void beginOperationTransaction(BeginOperationTransactionArgs e) {
-        super.beginOperationTransaction(e);
-
+    public void beforeExecuteOperationTransaction(BeforeOperationArgs e) {
+        super.beforeExecuteOperationTransaction(e);
         DynamicObject[] dataEntities = e.getDataEntities();
         for (DynamicObject dataEntity : dataEntities) {
             Long bizorg = (Long) dataEntity.getDynamicObject("bizorg").getPkValue();
             Long deliverchannel = (Long) dataEntity.getDynamicObject("nckd_deliverchannel").getPkValue();
             Date biztime = dataEntity.getDate("biztime");
             Long supplierId = this.getSupplierId(deliverchannel);
-            BigDecimal purContractPrice = this.getPurContractPrice(bizorg, supplierId,biztime);
-            if(purContractPrice.compareTo(BigDecimal.ZERO) > 0){
-                DynamicObjectCollection billentry = dataEntity.getDynamicObjectCollection("billentry");
-                for (DynamicObject entryRow : billentry) {
-                    entryRow.set("nckd_pricefielpsf",purContractPrice);
+            BigDecimal purContractPrice = this.getPurContractPrice(bizorg, supplierId, biztime);//配送费金额
+            DynamicObjectCollection billentry = dataEntity.getDynamicObjectCollection("billentry");
+            for (DynamicObject entryRow : billentry) {
+                entryRow.set("nckd_pricefielpsf", purContractPrice);
+                BigDecimal qty = entryRow.getBigDecimal("qty");
+                BigDecimal price = entryRow.getBigDecimal("nckd_pricefield3");
+                BigDecimal amountandtax = entryRow.getBigDecimal("amountandtax");
+                //溢价金额=价税合计-（数量*标准基价）
+                BigDecimal premiumamount = amountandtax.subtract(qty.multiply(price));
+                if (premiumamount.compareTo(BigDecimal.ZERO) > 0) {
+                    entryRow.set("nckd_premiumamount", premiumamount);
+                    entryRow.set("nckd_remainpayamount", premiumamount);
+                    //配送结算基本数量=溢价金额/配送单价
+                    entryRow.set("nckd_basicqtyps", purContractPrice.compareTo(BigDecimal.ZERO) > 0 ? premiumamount.divide(purContractPrice) : BigDecimal.ZERO);
+                } else {
+                    entryRow.set("nckd_premiumamount", 0);
+                    entryRow.set("nckd_remainpayamount", 0);
+                    entryRow.set("nckd_basicqtyps", 0);
                 }
-            }
 
+
+            }
         }
+        SaveServiceHelper.update(dataEntities);
 
     }
-    public BigDecimal getPurContractPrice(Long org, Long supplierId,Date biztime) {
+
+
+    public BigDecimal getPurContractPrice(Long org, Long supplierId, Date biztime) {
         QFilter qFilter = new QFilter("org", QCP.equals, org)
                 .and("supplier", QCP.equals, supplierId)//供应商;
-                .and("validstatus",QCP.equals,"B")
+                .and("validstatus", QCP.equals, "B")
                 .and("billstatus", QCP.equals, "C")
                 .and("closestatus", QCP.equals, "A")
-                .and("biztimeend",QCP.large_equals,biztime)
-                .and("biztimebegin",QCP.less_equals,biztime);
+                .and("biztimeend", QCP.large_equals, biztime)
+                .and("biztimebegin", QCP.less_equals, biztime);
         DynamicObjectCollection conm_purcontract = QueryServiceHelper.query("conm_purcontract", "billentry.priceandtax as priceandtax,org,supplier"
                 , new QFilter[]{qFilter}, "biztimebegin desc");
-        if (conm_purcontract == null || conm_purcontract.isEmpty()){
+        if (conm_purcontract == null || conm_purcontract.isEmpty()) {
             return BigDecimal.ZERO;
         }
         return conm_purcontract.get(0).getBigDecimal("priceandtax");
@@ -78,13 +102,13 @@ public class SalOutSaveOperationPlugIn extends AbstractOperationServicePlugIn im
         long customer = ocdbd_channel.getLong("customer");
         DynamicObject bd_customer = QueryServiceHelper.queryOne("bd_customer", "bizpartner",
                 new QFilter[]{new QFilter("id", QCP.equals, customer)});
-        if(bd_customer == null){
+        if (bd_customer == null) {
             return 0L;
         }
         long bizpartner = bd_customer.getLong("bizpartner");
         DynamicObject bd_bizpartner = QueryServiceHelper.queryOne("bd_supplier", "id",
                 new QFilter[]{new QFilter("bizpartner", QCP.equals, bizpartner)});
-        if (bd_bizpartner == null){
+        if (bd_bizpartner == null) {
             return 0L;
         }
         return bd_bizpartner.getLong("id");
