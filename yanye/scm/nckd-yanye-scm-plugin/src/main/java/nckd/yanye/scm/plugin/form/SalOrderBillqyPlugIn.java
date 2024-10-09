c0f46a1906ead38423d151896465286012ef5e69
huang920423 <460584688@qq.com>
2024-10-09 16:10:07 +0800

    需求申请辅助属性写值

--- yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/form/SalOrderBillqyPlugIn.java
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/form/SalOrderBillqyPlugIn.java
@@ -2,10 +2,16 @@ package nckd.yanye.scm.plugin.form;
 
 import kd.bos.bill.AbstractBillPlugIn;
 import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.entity.datamodel.RowDataEntity;
+import kd.bos.entity.datamodel.events.AfterAddRowEventArgs;
 import kd.bos.entity.datamodel.events.PropertyChangedArgs;
 //import org.joda.time.DateTime;
+import java.math.BigDecimal;
 import java.util.*;
 
+import kd.bos.entity.property.EntryProp;
+import kd.bos.form.IFormView;
 import kd.bos.metadata.dao.MetaCategory;
 import kd.bos.metadata.dao.MetadataDao;
 import kd.bos.metadata.entity.EntityMetadata;
@@ -13,8 +19,11 @@ import kd.bos.metadata.entity.businessfield.BasedataField;
 import kd.bos.metadata.form.ControlAp;
 import kd.bos.metadata.form.FormMetadata;
 import kd.bos.metadata.form.control.EntryFieldAp;
+import kd.bos.orm.query.QCP;
+import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.BusinessDataServiceHelper;
-
+import kd.fi.ap.helper.BaseDataHelper;
+import kd.bos.dataentity.utils.StringUtils;
 /**
  * 销售订单勾选清运供应商，根据商务伙伴基础资料自动获取，同时行类别需要同步为“服务（数量）”
  * 表单插件  nckd_sm_salorder_ext
@@ -36,74 +45,157 @@ public class SalOrderBillqyPlugIn extends AbstractBillPlugIn {
         super.propertyChanged(e);
         String resut="";
         String propName = e.getProperty().getName();
-        //获取表头的需求部门字段
-        DynamicObject dept = (DynamicObject) this.getModel().getValue("dept");
-        Object deptId = dept.getPkValue();
+//        //获取表头的需求部门字段
+//        DynamicObject dept = (DynamicObject) this.getModel().getValue("dept");
+//        Object deptId = dept.getPkValue();
 
         //订货客户
         if (propName.equals("customer")) {
             //Date billDate = DateTime.now().toDate();
 
-            DynamicObject customer = (DynamicObject) this.getModel().getValue("customer");
-            if (customer == null) return;
-            //客户Id
-            Object customerId = customer.getPkValue();
-            customer=BusinessDataServiceHelper.loadSingle(customerId,customer.getDynamicObjectType().getName());
+            String ynqy= this.getModel().getValue("nckd_xsqy").toString();
+
+            if("true".equals(ynqy)) {
+                DynamicObject customer = (DynamicObject) this.getModel().getValue("customer");
+                if (customer == null) return;
+                //客户Id
+                Object customerId = customer.getPkValue();
+                customer = BusinessDataServiceHelper.loadSingle(customerId, customer.getDynamicObjectType().getName());
+
+                String custname = customer.getString("name");
 
 
+                //查询客户，找到商务伙伴ID
+                DynamicObject customerDy = BusinessDataServiceHelper.loadSingle(customerId, "bd_customer");
+                DynamicObject FBizPartner = customerDy.getDynamicObject("bizpartner");
+                Object FBizPartnerID = FBizPartner.getPkValue();
 
-            String  entityKey="bd_bizpartner";
-            DynamicObject load = (DynamicObject) this.getModel().getValue(entityKey);
-            if(load == null)return;
-            String number = (String)load.get("number");
-            HashMap<String, String> map = new HashMap<>();
-            //功能：根据表单标识获取表单所有控件
-            //根据表单编码获取表单id
-            String id = MetadataDao.getIdByNumber(number, MetaCategory.Form);
-            //获取表单元数据
-            FormMetadata formMeta = (FormMetadata) MetadataDao.readRuntimeMeta(id, MetaCategory.Form);
-            //获取实体元数据
-            EntityMetadata metadata = (EntityMetadata) MetadataDao.readRuntimeMeta(id, MetaCategory.Entity);
-            // 表单元数据绑定实体元数据
-            formMeta.bindEntityMetadata(metadata);
-            //获取所有控件集合
-            List<ControlAp<?>> items = formMeta.getItems();
-            for (ControlAp<?> itemhb : items)
-            {
-                // 判断当前控件是否属于EntryFieldAp
-                if(itemhb instanceof EntryFieldAp)
-                {
-                    // 判断控件是否为基础资料控件
-                    if(((EntryFieldAp) itemhb).getField() instanceof BasedataField)
-                    {
-                        // 该步是存放基础资料多语言标识名称，以及标识
-                        map.put(itemhb.getName().getLocaleValue(),itemhb.getKey());
-                    }
+                //根据商务伙伴ID查询供应商
+                DynamicObject sup = BusinessDataServiceHelper.loadSingle("bd_supplier", new QFilter[]{new QFilter("bizpartner", QCP.equals, FBizPartnerID)});
+                if (sup == null) return;
+                String supid = sup.getString("id");
+
+
+                this.getModel().setValue("nckd_qygys", supid);
+
+                DynamicObjectCollection billentry = this.getModel().getEntryEntity("billentry");
+                int entryRowCount = this.getModel().getEntryRowCount("billentry");
+                for (int i = 0; i < entryRowCount; i++) {
+
+                    //查询行类型为020
+                    DynamicObject linetypeDy = BusinessDataServiceHelper.loadSingle("bd_linetype", new QFilter[]{new QFilter("number", QCP.equals, "020")});
+                    Object linetype = linetypeDy.getPkValue();
+
+                    getModel().setValue("linetype", linetype, i);
                 }
             }
 
-
-
         }
+
+
+        //销售清运
+        if (propName.equals("nckd_xsqy")) {
+            //Date billDate = DateTime.now().toDate();
+
+
+           String ynqy= this.getModel().getValue("nckd_xsqy").toString();
+
+            if("true".equals(ynqy)) {
+                DynamicObject customer = (DynamicObject) this.getModel().getValue("customer");
+                if (customer == null) return;
+                //客户Id
+                Object customerId = customer.getPkValue();
+                customer = BusinessDataServiceHelper.loadSingle(customerId, customer.getDynamicObjectType().getName());
+
+                String custname = customer.getString("name");
+
+
+                //查询客户，找到商务伙伴ID
+                DynamicObject customerDy = BusinessDataServiceHelper.loadSingle(customerId, "bd_customer");
+                DynamicObject FBizPartner = customerDy.getDynamicObject("bizpartner");
+                Object FBizPartnerID = FBizPartner.getPkValue();
+
+                //根据商务伙伴ID查询供应商
+                DynamicObject sup = BusinessDataServiceHelper.loadSingle("bd_supplier", new QFilter[]{new QFilter("bizpartner", QCP.equals, FBizPartnerID)});
+                if (sup == null) return;
+                String supid = sup.getString("id");
+
+
+                this.getModel().setValue("nckd_qygys", supid);
+
+                DynamicObjectCollection billentry = this.getModel().getEntryEntity("billentry");
+                int entryRowCount = this.getModel().getEntryRowCount("billentry");
+                for (int i = 0; i < entryRowCount; i++) {
+
+                    //查询行类型为020
+                    DynamicObject linetypeDy = BusinessDataServiceHelper.loadSingle("bd_linetype", new QFilter[]{new QFilter("number", QCP.equals, "020")});
+                    Object linetype = linetypeDy.getPkValue();
+
+                    getModel().setValue("linetype", linetype, i);
+                }
+            }
+            else
+            {
+                this.getModel().setValue("nckd_qygys", null);
+
+                DynamicObjectCollection billentry = this.getModel().getEntryEntity("billentry");
+                int entryRowCount = this.getModel().getEntryRowCount("billentry");
+                for (int i = 0; i < entryRowCount; i++) {
+
+                    //查询行类型为020
+                    DynamicObject linetypeDy = BusinessDataServiceHelper.loadSingle("bd_linetype", new QFilter[]{new QFilter("number", QCP.equals, "010")});
+                    Object linetype = linetypeDy.getPkValue();
+
+                    getModel().setValue("linetype", linetype, i);
+                }
+
+            }
+        }
+
+        //销售订单单据体【产品类型】字段如果为特殊包装产品，单据头存在特殊包装字段勾选
         if (propName.equals("material")) {
             //获取当前行
             int index = e.getChangeSet()[0].getRowIndex();
 
             DynamicObject material = (DynamicObject) this.getModel().getValue("material",index);
             if (material == null) return;
-            //客户Id
+            //物料Id
             Object materialId = material.getPkValue();
-            material=BusinessDataServiceHelper.loadSingle(materialId,material.getDynamicObjectType().getName());
+            String producttype=  material.getString("nckd_regularpackaging1");//获取物料销售信息-产品类型字段值
 
-            String   producttype=this.getModel().getValue("nckd_regularpackaging",index).toString();
-            if("为特殊包装产品".equals(producttype))
+//            DynamicObject salinfo = BusinessDataServiceHelper.loadSingle( "bd_materialsalinfo","id,nckd_regularpackaging1",new QFilter[]{new QFilter("masterid", QCP.equals,materialId)});
+//            String producttype = salinfo.getString("nckd_regularpackaging1");
+
+            if("1".equals(producttype))
             {
-
+                this.getModel().setValue("nckd_tsbz","true");
             }
-
 
         }
 
     }
+    public void afterAddRow(AfterAddRowEventArgs e) {
+        // 添加、插入、复制新行完毕，给新行填写了默认值之后，触发此事件；
+        // 插件可以在此修改新行字段默认值，或者调整界面上控件的状态
+        if (StringUtils.equals("billentry", e.getEntryProp().getName())){
 
+ 
+            String ynqy= this.getModel().getValue("nckd_xsqy").toString();
+
+            if("true".equals(ynqy)) {
+                int entryRowCount = this.getModel().getEntryRowCount("billentry");
+                for (int i = 0; i < entryRowCount; i++) {
+
+                    //查询行类型为020
+                    DynamicObject linetypeDy = BusinessDataServiceHelper.loadSingle("bd_linetype", new QFilter[]{new QFilter("number", QCP.equals, "020")});
+                    Object linetype = linetypeDy.getPkValue();
+
+                    getModel().setValue("linetype", linetype, i);
+                }
+            }
+        }
+
+
+        // 实例：DataModelChangeListener捕获此事件，给前端单据体增加新行
+    }
 }
