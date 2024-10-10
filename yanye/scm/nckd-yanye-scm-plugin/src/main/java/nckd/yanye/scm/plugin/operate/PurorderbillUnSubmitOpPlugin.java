6dce33c384d2c65d756efbb45c0b11edffaf118b
zhujintao <983068902@qq.com>
2024-10-10 09:51:16 +0800

    modfiy:采购订单与采购合同之间的关联关系表在撤销的时候要删除，提交的时候写入

--- yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/operate/PurorderbillUnSubmitOpPlugin.java
+++ yanye/scm/nckd-yanye-scm-plugin/src/main/java/nckd/yanye/scm/plugin/operate/PurorderbillUnSubmitOpPlugin.java
@@ -1,10 +1,14 @@
 package nckd.yanye.scm.plugin.operate;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.ObjectUtil;
 import kd.bos.dataentity.entity.DynamicObject;
 import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.db.DB;
+import kd.bos.db.DBRoute;
 import kd.bos.entity.plugin.AbstractOperationServicePlugIn;
 import kd.bos.entity.plugin.PreparePropertysEventArgs;
+import kd.bos.entity.plugin.args.AfterOperationArgs;
 import kd.bos.entity.plugin.args.BeginOperationTransactionArgs;
 import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
@@ -106,5 +110,87 @@ public class PurorderbillUnSubmitOpPlugin extends AbstractOperationServicePlugIn
                 }
             }
         }
+    }
+
+    @Override
+    public void afterExecuteOperationTransaction(AfterOperationArgs e) {
+        super.afterExecuteOperationTransaction(e);
+        // 提交时校验
+        DynamicObject[] entities = e.getDataEntities();
+        Set<Long> conbillids = new HashSet<>();
+        Arrays.stream(entities).forEach(k -> {
+            DynamicObjectCollection billentry = k.getDynamicObjectCollection("billentry");
+            Set<Long> conbillidSet = billentry.stream().map(h -> h.getLong("conbillid")).collect(Collectors.toSet());
+            conbillids.addAll(conbillidSet);
+        });
+        //根据提交的采购订单去查询采购合同
+        QFilter qFilter = new QFilter("id", QCP.in, conbillids)
+                .and("billstatus", QCP.equals, "C")
+                .and("validstatus", QCP.equals, "B")
+                .and("closestatus", QCP.equals, "A");
+        //String entityName, String selectProperties, QFilter[] filters
+        DynamicObject[] purcontractArr = BusinessDataServiceHelper.load("conm_purcontract", "id,billno,totalallamount,nckd_totalallamount,nckd_totalallamountrat,billentry.id,billentry.material,billentry.qty,billentry.nckd_qty,billentry.nckd_qtyratio,billentry.amountandtax,billentry.nckd_amount,billentry.nckd_amountratio,billentry.seq", qFilter.toArray());
+        //转成key id value DynamicObject采购合同
+        Map<Long, DynamicObject> map = Arrays.stream(purcontractArr).collect(Collectors.toMap(k -> k.getLong("id"), v -> v));
+        //遍历提交的采购订单单据 进行逻辑处理
+        for (DynamicObject purorderbill : entities) {
+            DynamicObjectCollection billentry = purorderbill.getDynamicObjectCollection("billentry");
+            for (DynamicObject entry : billentry) {
+                //获取分录行的合同id
+                long key = entry.getLong("conbillid");
+                //获取采购合同
+                DynamicObject purcontractBill = map.get(key);
+                //获取到了才进行下面逻辑，否则下一条分录
+                if (ObjectUtil.isNotEmpty(purcontractBill)) {
+                    DynamicObjectCollection purcontractBillEntry = purcontractBill.getDynamicObjectCollection("billentry");
+                    //转换为key 为物料 value 为该行数据
+                    Map<Object, DynamicObject> purcontractBillEntryMap = purcontractBillEntry.stream().filter(h -> ObjectUtil.isNotEmpty(h.getDynamicObject("material"))).collect(Collectors.toMap(k -> k.getDynamicObject("material").getPkValue(), v -> v));
+                    //当前单据分录的物料
+                    Object materialId = entry.getDynamicObject("material").getPkValue();
+                    //以下字段来源于采购合同
+                    DynamicObject purcontractBillEntryDy = purcontractBillEntryMap.get(materialId);
+                    // 带参数
+                    boolean result = deleteTBotpBilltracker(purcontractBill.getPkValue(), purorderbill.getPkValue());
+                    if (result) {
+                        deleteTPmPurorderbillTc(purcontractBill.getPkValue(), purcontractBillEntryDy.getPkValue(), purorderbill.getPkValue(), entry.getPkValue());
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 删除规则表
+     */
+    private static void deleteTPmPurorderbillTc(Object sbillid, Object sid, Object tbillid, Object tid) {
+        String purorderBillTcSql = "DELETE FROM t_pm_purorderbill_tc WHERE ftbillid = ? AND fttableid = ? AND ftid = ? AND fsbillid = ? AND fstableid = ? AND fsid =? ;";
+        Long ftbillid = Convert.toLong(tbillid);
+        Long fttableid = 602924326097811460L;
+        Long ftid = Convert.toLong(tid);
+        Long fsbillid = Convert.toLong(sbillid);
+        Long fstableid = 719529409035381761L;
+        Long fsid = Convert.toLong(sid);
+        DB.execute(DBRoute.of("scm"), purorderBillTcSql, new Object[]{ftbillid, fttableid, ftid, fsbillid, fstableid, fsid});
+    }
+
+    /**
+     * 删除botp规则表
+     */
+    private static boolean deleteTBotpBilltracker(Object sbillid, Object tbillid) {
+        String botpSql = "DELETE FROM t_botp_billtracker WHERE fstableid = ? AND fsbillid = ? AND fttableid = ? AND ftbillid = ? ;";
+        Long fstableid = 719529409035381761L;
+        Long fsbillid = Convert.toLong(sbillid);
+        Long fttableid = 602924326097811460L;
+        Long ftbillid = Convert.toLong(tbillid);
+        Date fcreatetime = new Date();
+        //先判断有没有这个botp关系，有就不再添加
+        QFilter qFilter = new QFilter("stableid", QCP.equals, 719529409035381761L).and("sbillid", QCP.equals, sbillid)
+                .and("ttableid", QCP.equals, 602924326097811460L).and("tbillid", QCP.equals, tbillid);
+        DynamicObject botpBilltracker = BusinessDataServiceHelper.loadSingle("botp_billtracker", "id,stableid,sbillid,ttableid,tbillid,createtime", qFilter.toArray());
+        if (ObjectUtil.isNotEmpty(botpBilltracker)) {
+            return false;
+        }
+        boolean result = DB.execute(DBRoute.basedata, botpSql, new Object[]{fstableid, fsbillid, fttableid, ftbillid});
+        return result;
     }
 }
